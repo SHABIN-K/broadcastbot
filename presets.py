960f3b38b6a61d4a37a4b62f31dbdf281edc08f4
Shabin-k <73272797+ERICDANIYEL@users.noreply.github.com>
2021-08-22 14:59:01 +0530

    Add files via upload

---
+++ presets.py
@@ -0,0 +1,5 @@
+class Presets(object):
+
+    USERS_LIST = "<b>⭕️Total:</b>\n\n⭕️Subscribers - {}\n⭕️Blocked- {}"
+    WAIT_MSG = "<b>Processing ...</b>"
+    REPLY_ERROR = "<code>Use this command as a replay to any telegram message with out any spaces.</code>"
